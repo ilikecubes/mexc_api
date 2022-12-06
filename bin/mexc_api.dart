5614a5cd6d55e5accd5ce292a441e754cd18d17d
Kevin <scheeren@proton.me>
2022-12-06 16:15:00 +0100

    Initial Commit

---
+++ bin/mexc_api.dart
@@ -0,0 +1,5 @@
+import 'package:mexc_api/mexc_api.dart' as mexc_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mexc_api.calculate()}!');
+}
---
+++ test/mexc_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:mexc_api/mexc_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
